354a768a96455c8799a0de0dc271cb98e090f8d5
reza <reza@kali.reza>
2026-01-06 08:17:12 -0500

    registery for past and copy

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -27,3 +27,14 @@ map("n", "<C-l>", "<C-w>l")
 map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 
+-- Normal mode
+map('n', 'd', '"_d', { desc = "Delete without yanking" })
+map('n', 'D', '"_D', { desc = "Delete line without yanking" })
+map('n', 'c', '"_c', { desc = "Change without yanking" })
+map('n', 'C', '"_C', { desc = "Change line without yanking" })
+map('n', 's', '"_s', { desc = "Substitute without yanking" })
+map('n', 'S', '"_S', { desc = "Substitute line without yanking" })
+
+-- Visual mode
+map('v', 'd', '"_d', { desc = "Delete selection without yanking" })
+map('v', 'p', '"_dP', { desc = "Paste over selection without losing current pasteboard" })
